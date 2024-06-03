3193b717888d30521a402a384f27e5403447e134
Sajal1005 <nandasajal.208@gmail.com>
2024-06-03 21:00:16 +0530

    Patterns Ques Completed

--- Patterns/src/PatternsQuestions.java
+++ Patterns/src/PatternsQuestions.java
@@ -419,6 +419,14 @@ public class PatternsQuestions {
 //        }
 
 
-        
+//        23.
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0 || i==n-1 || j==n-1 || i==j || i==n-j-1){
+                    System.out.print("*");
+                }else System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
